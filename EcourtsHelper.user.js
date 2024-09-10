b36f3f10c9b4615c510e36e6933896848ac8c35f
Nick Kantack <kantackn@amazon.com>
2024-09-10 14:43:56 -0600

    Trying new strategy of clicking the keepAlive button regularly

--- EcourtsHelper.user.js
+++ EcourtsHelper.user.js
@@ -23,8 +23,42 @@
 
     // Schedule GET calls to the server to trigger a refresh of the session
     setInterval(() => {
-        KJSC.WebClient.loadUrlSync(window.location.href);
+        // The get request
+        /*
+        fetch(window.location.href, {
+            method: 'GET',
+            credentials: 'include' // Include cookies for session
+        }).then(response => {
+            // Handle response if needed
+            console.log('Session keep-alive request sent');
+        }).catch(error => {
+            console.error('Error sending keep-alive request:', error);
+        });
         console.log(`Refreshed session`);
+
+        // Try a mouse move
+        let mouseEvent = new MouseEvent('mousemove', {
+            bubbles: true,
+            cancelable: true
+        });
+        document.dispatchEvent(mouseEvent);
+
+        // Simulate focus
+        let focusedElement = document.activeElement || document.body;
+        focusedElement.focus();
+
+        // Click the nav bar
+        document.getElementById(`main-footer`).click();
+        */
+
+        // Calling the page's keepAlive() method
+        const staySignedInButton = document.getElementById(`SessionPop`).querySelector(`div.modal-footer button`);
+        console.log(staySignedInButton.innerHTML);
+        staySignedInButton.click();
+
+        console.log(`Interacted with page a bit to keep session alive`);
+
     }, 10000);
 
 })();
+
